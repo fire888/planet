34920ebef9087e266f9e89134d9afb30c4af5bc4
vasilii <porokh.vs@gmail.com>
2018-09-13 04:51:32 +0300

    add shader

--- www_src/index.js
+++ www_src/index.js
@@ -7,7 +7,8 @@
 let textureLoader,
 textures = {
   sceneBack: null,
-  waterNormals: null
+  waterNormals: null,
+  continents: null
 }
 
 const loadAssets = ( onLoad ) => {
@@ -28,6 +29,12 @@ const loadAssets = ( onLoad ) => {
             }
         )	
   }) )
+  .then( () => new Promise ( ( resolve ) => {
+        textures.continents = textureLoader.load( 
+            'assets/contour.jpg',
+            () => { resolve() }
+        )	
+  }) )  
   .then( () => { 
     textureLoader = null
     onLoad() } 
@@ -49,9 +56,9 @@ const initScene = () => {
   camera.position.set( -800, 0, 1800 )   
 
   let pointL = new THREE.PointLight( 0xffffff, 2.0 )
-  pointL.position.set( -400, 1000, 1600 )
+  pointL.position.set( 1000, 1000, 1000 )
   scene.add( pointL )
-  let lightAmb = new THREE.AmbientLight( 0xffffff, 0.5 )
+  let lightAmb = new THREE.AmbientLight( 0xffffff, 0.2 )
   scene.add( lightAmb )
 
   renderer = new THREE.WebGLRenderer( { alpha: true, canvas: document.getElementById( 'webGL' ) } )
@@ -71,11 +78,11 @@ const drawFrame = () => {
 /*******************************************************************/
 
 const addOnWindowResize = () => {
-  window.addEventListener('resize', handleWindowResize, false)
+  window.addEventListener( 'resize', handleWindowResize, false )
 } 
 
 const handleWindowResize = () => {
-  renderer.setSize(window.innerWidth, window.innerHeight)
+  renderer.setSize( window.innerWidth, window.innerHeight )
   camera.aspect = window.innerWidth / window.innerHeight
   camera.updateProjectionMatrix()
 }
@@ -100,7 +107,7 @@ const onMouseWheel = () => {
       let slogan = document.getElementById( 'slogan' )
       slogan.className = 'show' 
     }, 500 )  
-  window.removeEventListener('wheel', onMouseWheel, false)
+  window.removeEventListener( 'wheel', onMouseWheel, false )
 }
 
 
@@ -108,17 +115,20 @@ const onMouseWheel = () => {
 
 /********************************************************************/
 
-let earth
+let earth, globeMesh, continentsMesh
 
 const createEarth = () => {
   earth = new THREE.Group()
-  earth.add( createGlobe() )
+  globeMesh = createGlobe() 
+  earth.add( globeMesh )
+  continentsMesh = createContinents() 
+  earth.add( continentsMesh )
   scene.add( earth )
 }
 
 const createGlobe = () => {
   return new THREE.Mesh( 
-    new THREE.SphereGeometry( 600, 30, 30 ),
+    new THREE.SphereGeometry( 600, 40, 40 ),
     new THREE.MeshPhongMaterial( {
       map: textures.waterNormals,
       normalMap: textures.waterNormals,
@@ -130,6 +140,22 @@ const createGlobe = () => {
   )
 }
 
+const createContinents = () => {
+  let mesh =  new THREE.Mesh( 
+    new THREE.SphereGeometry( 608, 40, 40 ),
+    new THREE.ShaderMaterial( continentsShader )
+  )  
+  mesh.rotation.z = -0.5
+  mesh.rotation.x = -0.5
+  mesh.material.transparent = true
+  mesh.material.side = THREE.DoubleSide
+  mesh.material.depthWrite = false
+  mesh.material.needsUpdate = true
+  mesh.material.uniforms.tDiffuse.value = textures.continents
+  return mesh
+}
+
+
 let earthSpd = 0.002, 
 earthDir = 'left' // || 'right' || 'normal'  
 
@@ -139,9 +165,15 @@ const updateAnimationEarth = () => {
   if ( earthSpd < -0.005 && earthDir == 'right' ) earthDir = 'left'  
   if ( earthDir == 'left' ) earthSpd += 0.0001
   if ( earthDir == 'right') earthSpd -= 0.0001
-  if ( earthDir == 'normal' && earthSpd < 0.02 ) earthSpd += 0.0001 
+  if ( earthDir == 'normal' && earthSpd < 0.02 ) { 
+    if ( continentsMesh.material.uniforms.light.value < 1.35 ) {
+      continentsMesh.material.uniforms.light.value += 0.012
+      globeMesh.material.opacity -= 0.0005
+      globeMesh.material.needsUpdate = true
+    }  
+    earthSpd += 0.00005
+  }   
   earth.rotation.y += earthSpd
-  earth.rotation.z += earthSpd*0.2
 }
 
 const setSpeedEarthNormal = () => earthDir = 'normal'
@@ -162,7 +194,50 @@ window.onload = () => {
     hidePreloader()
     startCheckMouseWheell()   
   } )
-}  
+}
+
+
+
+
+/*******************************************************************/
+/*******************************************************************/
+/*******************************************************************/
+
+const continentsShader = {
+  uniforms: {		
+	  'tDiffuse' : { value: null },
+    'light': { value: 0.2 },       	
+  },
+  vertexShader: [	
+	  'varying vec2 vUv;',
+	
+	  'void main() {',
+      'vUv = uv;',
+      'gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );',
+    '}'
+  ].join( '\n' ),	
+  fragmentShader: [
+    '#ifdef GL_ES',
+    'precision highp float;',
+    '#endif',
+
+    'uniform highp sampler2D tDiffuse;', 
+    'uniform float light;', 
+
+    'varying vec2 vUv;',
+
+    'void main() {',	
+      'vec2 uv = vUv;',
+
+      "vec4 diff = texture2D(tDiffuse, uv);",
+      "vec4 cont = vec4( 0.7*light, 0.7*light, 1.0*light,  diff.x*light*1.3 );",
+      "vec4 ground = vec4( 0.01*light-0.5, 0.01*light-0.5, 0.6*light,  (1.0 - diff.y)*light*0.6 );",
+
+      "gl_FragColor = cont + ground;",
+    '}'
+  ].join( "\n" )
+}
+
 
 
 
